f9d41dd1b753c9015e9c1ba94c79091d1a988ce6
Christian Gonzalez <cgonzalez233@yahoo.com>
2022-03-24 15:24:43 -0400

    added user/order services

--- shopping-app-project/src/app/components/category-page/category-page.component.ts
+++ shopping-app-project/src/app/components/category-page/category-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import{DataServiceService} from 'src/app/data-service.service'
+import{DataService} from 'src/app/data.service'
 import{ICategories}from 'src/app/components/category-page/CategoryC'
 import { Router } from '@angular/router';
 
@@ -15,13 +15,15 @@ export class CategoryPageComponent implements OnInit {
     'https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg',
     'https://fakestoreapi.com/img/51Y5NI-I5jL._AC_UX679_.jpg',
     'https://fakestoreapi.com/img/61IBBVJvSDL._AC_SY879_.jpg',
-    'https://fakestoreapi.com/img/71YAIFU48IL._AC_UL640_QL65_ML3_.jpg'];
-    
-  constructor(private http: DataServiceService, private route: Router) {}
+    'https://fakestoreapi.com/img/71YAIFU48IL._AC_UL640_QL65_ML3_.jpg'
+  ];
+
+
+  constructor(private http: DataService, private route: Router) {}
   
 
   ngOnInit(): void {
-    this.http.getAllCategories().subscribe(data=>this.categories = data)
+    this.http.getCategories().subscribe(data=>this.categories = data)
   }
   catPass(category: string){
     this.route.navigate(["name", category])
--- shopping-app-project/src/app/data.service.spec.ts
+++ shopping-app-project/src/app/data.service.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 
 import { DataService } from './data.service';
 
-describe('DataServiceService', () => {
+describe('DataService', () => {
   let service: DataService;
 
   beforeEach(() => {
