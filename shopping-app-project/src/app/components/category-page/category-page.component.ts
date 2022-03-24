c5d3e5087cc91d742e31327e0b1e4aa0c3a43baf
Christian Gonzalez <cgonzalez233@yahoo.com>
2022-03-24 11:49:37 -0400

    update to category ts

--- shopping-app-project/src/app/components/category-page/category-page.component.ts
+++ shopping-app-project/src/app/components/category-page/category-page.component.ts
@@ -10,13 +10,15 @@ import { Router } from '@angular/router';
 })
 export class CategoryPageComponent implements OnInit {
  
-  public categories: String[];
-  public images: String[];
-  constructor(private http: DataServiceService, private route: Router) {
-    this.categories = [];
-    this.images = ['https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg',
-    'https://fakestoreapi.com/img/51Y5NI-I5jL._AC_UX679_.jpg','https://fakestoreapi.com/img/61IBBVJvSDL._AC_SY879_.jpg','https://fakestoreapi.com/img/71YAIFU48IL._AC_UL640_QL65_ML3_.jpg']
-   }
+  public categories: any[] = [];
+  public images: any [] = [
+    'https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg',
+    'https://fakestoreapi.com/img/51Y5NI-I5jL._AC_UX679_.jpg',
+    'https://fakestoreapi.com/img/61IBBVJvSDL._AC_SY879_.jpg',
+    'https://fakestoreapi.com/img/71YAIFU48IL._AC_UL640_QL65_ML3_.jpg'];
+    
+  constructor(private http: DataServiceService, private route: Router) {}
+  
 
   ngOnInit(): void {
     this.http.getAllCategories().subscribe(data=>this.categories = data)
