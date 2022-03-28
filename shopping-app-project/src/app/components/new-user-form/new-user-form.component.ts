81b4236a54ccfd9c0b2fb0a2d613cbeee0beab12
Shawn Boutte <sbboo22@gmail.com>
2022-03-27 19:02:20 -0500

    input validation

--- shopping-app-project/src/app/components/new-user-form/new-user-form.component.ts
+++ shopping-app-project/src/app/components/new-user-form/new-user-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/data.service';
+import {User} from 'src/app/models/user'
 
 @Component({
   selector: 'app-new-user-form',
@@ -7,7 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewUserFormComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: DataService) { 
+    
+  }
+  public user: any;
+  users: User[];
+  public message: string;
+  public username: string  = "";
+  public email: string ="";
+  public password: string = "";
+  public address: string = "";
+  public phone: string = "";
+  addUser(){
+      this.user = new User(this.username, this.password, this.email, this.address, this.phone)
+      this.http.getUsers().subscribe(data=>this.users = data);
+      this.users.forEach(user => {
+      if(user.username === this.username || user.password === this.password){
+        this.message = 'Repeat creds';
+      }
+      })
+      if(this.message == 'Repeat creds'){ 
+        alert(this.message)
+        this.message = "";
+      }
+      else{
+        this.http.createUser(this.user).subscribe(data=>this.user = data);
+      }
+  }
 
   ngOnInit(): void {
   }
